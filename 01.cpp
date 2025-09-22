d4f0ea21ba845b55f945c6cf491d957373c648ea
balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-09-22 15:10:48 +0200

    base

---
+++ 01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main(){
+
+
+    SetConsoleCP(1250);
+    SetConsoleOutputCP(1250);
+    int i = 0;
+    char c = 'c';
+    int j [] ={1,2,3,4,5};
+    long long q = 123123121212;
+    signed char i1;
+    unsigned char i2;
+
+    //a
+    /* asd */
+    cout << "Hello World" <<  endl;
+
+    return 0;
+
+}
