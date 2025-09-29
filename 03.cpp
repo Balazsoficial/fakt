47aa84436924236febb57b434b564863fe16549b
balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-09-29 16:31:24 +0200

    masodik ora

---
+++ 03.cpp
@@ -0,0 +1,28 @@
+//
+// Created by szoldaticsnagybalazs on 2025.09.29.
+//
+#include <cmath>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    double x = 3.1415926;
+    cout  << setprecision(8) << x << endl;
+    cout << round(1000*x)/1000 << endl;
+
+    cout << trunc(x) << endl;
+    //--
+    cout << " irj be szoveget " << endl;
+    string s;
+    cin >> s;
+    cout << s << endl;
+    cin.ignore(256, '\n');
+    getline(cin, s);
+    cout << s << endl;
+
+
+}
---
+++ 04.cpp
@@ -0,0 +1,21 @@
+//
+// Created by szoldaticsnagybalazs on 2025.09.29.
+//
+#include<iostream>
+
+using namespace  std;
+int main() {
+    string s;
+    string mah="öüóőúaűáéeAŰÁÉEuUOÓÜÖÚŐíÍ";
+    cout << "kerek szoveget " << endl;
+    getline(cin, s);
+    for (int i =0; i<s.length(); i++) {
+        if (mah.find(s[i])<=mah.length()) {
+            cout << mah.find(s[i])<<endl;
+            s[i] ='*';
+        }
+
+    }
+    cout << s << endl;
+
+}
