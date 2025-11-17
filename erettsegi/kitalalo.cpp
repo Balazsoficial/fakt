8072dca7a0f8d6be3a3d6c74cc60ff453a38c2b1
balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-11-17 16:24:30 +0100

    ora

--- erettsegi/kitalalo.cpp
+++ erettsegi/kitalalo.cpp
@@ -1,3 +1,39 @@
 //
 // Created by Balazsofficial on 2025.11.17.
 //
+#include <iostream>
+using namespace std;
+int main() {
+    string szavak[15]=  {"fuvola", "csirke", "adatok", "asztal", "fogoly", "bicska", "farkas", "almafa", "babona", "gerinc", "dervis", "bagoly", "ecetes", "angyal", "boglya"};
+    srand(time(NULL));
+    int v = rand() % 15;
+    string s = szavak[v];
+    int db =0;
+    int ts=0;
+    string tipp;
+    while(db<6  && tipp!="stop") {
+        cout << "Kerem a tippet: ";
+        cin >> tipp;
+        if(tipp!="stop") {
+            ts++;
+            string eredmeny ="";
+            for(int i=0;i<6;i++) {
+                if (s[i] == tipp[i]){                    db++;
+                    eredmeny+=s[i];}
+
+                else {
+
+                    eredmeny+=".";
+                }
+
+            }
+            cout <<"eredmeny: " <<eredmeny << endl;
+        }
+    }
+    if (db==6)
+        cout <<ts<< "Tippelessel sikerult kitalalni!! " << endl;
+
+
+
+
+}
---
+++ erettsegi/szallitas.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Balazsofficial on 2025.11.17.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int szamok[15] = {16, 8, 9, 4, 3, 2, 4, 7, 7, 12, 3, 5, 4, 3, 2};
+    int osszt;
+    for (int i = 0; i < 15; i++) {
+        osszt+=szamok[i];
+    }
+    cout << "A targyak tomegenek osszege: "<< osszt<< "kg " << endl;
+
+    int dobozok=0;
+    int dobozc=0;
+
+    for (int i = 0; i < 15; i++) {
+        dobozc+=szamok[i];
+        if (i!=14 && dobozc+szamok[i+1] <=20){}
+
+        else {
+            cout << dobozc << " "  ;
+            dobozok++;
+            dobozc=0;
+        }
+
+    }
+    cout <<"A szukseges dobozok szama: " << dobozok;
+}
