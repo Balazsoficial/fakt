e44b43c9585211dbb24eb284455465e59a0a3d93
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-11-21 08:44:27 +0100

    ora

--- erettsegi/befozes.cpp
+++ erettsegi/befozes.cpp
@@ -1,3 +1,34 @@
 //
 // Created by Balazsoficial on 2025.11.21.
 //
+#include <iostream>
+using namespace std;
+int main() {
+    int uvegek[15]={5, 2, 2, 4, 3, 2, 4, 10, 5, 5, 3, 5, 4, 3, 3};
+    int L =0;
+    cout << "2. feladat" << endl;
+    do {
+        cout << "Mari neni lekvarja(dl):";
+        cin >> L;
+
+    }while (0>L>200);
+    int legnagyobb,hanyadik;
+    for(int i=0;i<15;i++) {
+        if (uvegek[i]>legnagyobb)
+        {        legnagyobb = uvegek[i];
+            hanyadik = i;}
+            }
+    cout << "3. feladat" << endl;
+    cout << "A legnagyobb uveg:" << legnagyobb <<" dl es "<<hanyadik<<". a sorban"<< endl;
+    cout <<"4. feladat" << endl;
+    int osszeg=0;
+    for(int i=0;i<15;i++) {
+
+        osszeg+=uvegek[i];
+
+    }
+    if (osszeg >L)
+        cout << "Elegendo uveg volt " <<endl;
+    else
+        cout << "Maradt lekvar" <<endl;
+}
