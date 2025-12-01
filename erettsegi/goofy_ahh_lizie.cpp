c26e5e145f0fc8ca2a2cefeb255669753dce62dc
balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-12-01 15:12:10 +0100

    ora

---
+++ erettsegi/goofy_ahh_lizie.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Balazsoficial on 2025.11.26.
+//
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+int x,y;
+
+vector <int> sziget [1][1]{};
+
+int main() {
+    x=y=0;
+    ifstream fin;
+    fin.open("S:/11/FAKT/cpp/erettsegi/sziget.txt");
+    string s;
+    char sx;
+    char sy;
+    getline(fin, s);
+
+
+
+    cout << x << " " << y << endl;
+
+
+
+
+
+}
