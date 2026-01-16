2b8e867a659af283874194f2eaa632aecf20eebe
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2026-01-16 08:45:17 +0100

    ora

--- erettsegi/lotosz2005.cpp
+++ erettsegi/lotosz2005.cpp
@@ -7,14 +7,62 @@
 #include <vector>
 
 using namespace std;
-char a =' ' ;
+int szamok[100];
+int a =0 ;
 struct lista {
     int num[5]={};
 };
+lista l[52] ={};
 
+
+int volte() {
+    for(int i=0;i<52;i++) {
+        for(int j=0;j<5;j++) {
+            if (szamok[l[i].num[j]]==0) {
+                szamok[l[i].num[j]] =1;
+
+
+            }
+
+        }
+
+    }
+    bool volte =false; //olyan amit nem huztak ki
+    for (auto i : szamok) {
+        if (szamok[i]==0) {
+            volte = true;
+        }
+
+    }
+    return volte;
+
+}
+int paratlansum() {
+    for(int i=0;i<52;i++) {
+        for(int j=0;j<5;j++) {
+            if (szamok[l[i].num[j]]==0) {
+                szamok[l[i].num[j]] =1;
+
+
+            }
+
+        }
+
+    }
+   int db=0; //olyan amit nem huztak ki
+    for (auto i : szamok) {
+        if (szamok[i]==1 && i % 2 ==1) {    //rossu
+            db ++;
+        }
+
+    }
+    return db;
+
+}
 int main() {
-    lista l[51] ={};
+
     int lottosz[5]={};
+    cout << "Add meg az 52. het lottoszamait! "<< endl;
     cin>>lottosz[0]>>lottosz[1]>>lottosz[2]>>lottosz[3]>>lottosz[4];
     sort(lottosz,lottosz+5);
 
@@ -34,7 +82,16 @@ int main() {
     for(int i=0;i<5;i++) {
         cout<<l[het-1].num[i]<<" ";
     }
-cin >>a;
+
+    cout << endl;
+//5
+    if (volte()) {
+        cout << "Volt olyan amit nem huztak ki az 51 hetz alatt" << endl;
+
+    }
+    //6
+    cout << paratlansum()  << " darab paratlan volt"<< endl;
+
 
 
 }
--- konyvkiadas.cpp
+++ konyvkiadas.cpp
@@ -3,7 +3,7 @@
 //
 #include <iostream>
 #include <fstream>
-
+//2025 majus
 using namespace std;
 struct adat {
         int ev,negyed;
@@ -29,18 +29,110 @@ void f1() {
         k[n].ered=s;
         getline(f,s,';');
         k[n].nev=s;
-        getline(f,s,';');
+        getline(f,s);
         k[n].db=stoi(s);
+        getline(f,s, ';');
     }
 }
+void f2() {
+    cout << "masodik feladat" <<endl;
+    cout <<"szerzo: " <<endl;
+    string szerzo;
+    getline(cin,szerzo);
+    int kdb =0;
+    for (int i=0;i<n;i++)
+    if (k[i].nev.find(szerzo)<k[i].nev.length()) {
+        kdb++;
+    }
+    if (kdb>0) {
+        cout << kdb << " Konyv kiadas" <<endl;
+    }
+else
+    cout << "Nem adtak ki" << endl;
 
-int main() {
-    f1();
-    for(int i=0;i<11;i++) {
-        cout << k[i].ev << " " << k[i].negyed << "  " << k[i].ered <<  "    " << k[i].nev << "  " <<k[i].db<< endl;
+}
+void f3() {
+    cout << "3. feladat " <<endl;
+    int maxpld = k[1].db ;
+    int maxdb =1;
+    for (int i=0;i<n;i++) {
+        if (k[i].db==maxpld) {
+            maxdb++;
+        }
+        else {
+
+            if (k[i].db>maxpld) {
+                maxpld=k[i].db;
+                maxdb=1;
+            }
+
+        }
+
+
 
     }
-    cout << n << endl;
+    cout << "Legnagyobb peldanyszam: " << maxpld << " Elofordult: "<< maxdb  << "alkalommal"<<endl;
+
+}
+void f4() {
+    cout <<"4. feladat" << endl;
+    int i =1;
+    while (k[i].db<40000 || k[i].ered!="kf") {
+        i++;
+    }
+    cout << k[i].ev << "/" << k[i].negyed << ". " << k[i].nev << endl;
+}
+void f5() {
+    cout << "5 feladat" << endl;
+    struct stat{
+        int ev,mdb,mpld,kdb,kpld;
+
+    };
+    stat st[4];
+
+    for (int i=0;i<4;i++) {
+        st[i].ev = 2020+i;
+        st[i].mdb = st[i].mpld = st[i].kdb = st[i].kpld = 0;
+    }
+    for (int i=1;i<=n;i++) {
+        if (k[i].ered=="ma") {
+            st[k[i].ev-2020].mdb++;
+            st[k[i].ev-2020].mpld+=k[i].db;
+
+        }
+
+        else {
+            st[k[i].ev-2020].kdb++;
+            st[k[i].ev-2020].kpld+=k[i].db;
+
+
+
+
+        }
+
+    }
+    cout <<"Ev \tMagyar kiadas \tMagyar peldanyszam \tKulfoldi kiadas \tkulfoldi peldanyszam" <<endl;
+    for (int i=1;i<=3;i++) {
+        cout <<st[i].ev <<"\t\t" <<st[i].mdb <<"\t\t" <<st[i].mpld <<"\t\t\t" <<st[i].kdb <<"\t\t\t" <<st[i].kpld <<endl;
+    }
+
+    //HTML
+    ofstream fout("tabla.html");
+    fout << "<table> <tr><th>Év</th><th>Magyar kiadás</th><th>Magyar példányszám</th><th>Külföldi kiadás</th><th>Külföldi példányszám</th></tr>"<<endl;
+    for (int i=1;i<=3;i++) {
+        fout << "<tr><td>"<<st[i].ev <<"</td><td>"<<st[i].mdb<<"</td><td>"<<st[i].mpld<<"</td><td>"<<st[i].kdb<<"</td><td>"<<st[i].kpld<<"</td></tr> " << endl;
+    }
+    fout << "</table>" << endl;
+    fout.close();
+}
+int main() {
+    f1();
+    f2();
+    f3();
+    f4();
+    f5();
+
+   // cout << n << endl;
 
 
 }
